4334903ce081f85d0a5fe5faeab8368188305ca9
Iosif Osman <iosif.osman@gmail.com>
2021-12-12 14:50:06 +0200

    Final Modifications

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var principal_number = parseInt(principal);
+    if (isNaN(principal_number) || principal_number<=0) {
+        alert("Enter a positive number")
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = parseInt(principal)*parseInt(years)*parseInt(rate)/100
+    var year = new Date().getFullYear() + parseInt(years)
     
+    document.getElementById("result").innerHTML="If you deposit \<b\>"+principal+"\</b\>,\<br\>at an interest rate of \<b\>"+rate+"%\</b\>.\<br\>You will receive an amount of \<b\>"+interest+"\</b\>,\<br\>in the year \<b\>"+year+"\</b\>\<br\>"
+}
+
+function updateRate() {
+    var rateVal = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateVal;
 }
         
